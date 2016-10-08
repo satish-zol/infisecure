0505709600796a3472c7d61b285b7a3173a3993a
Satish <zsatish201@gmail.com>
2016-10-08 20:02:27 +0530

    fix broken hash

--- lib/generators/infisecure/infisecure.rb
+++ lib/generators/infisecure/infisecure.rb
@@ -2,7 +2,7 @@ Infisecure.configure do |config|
   file = File.read("#{Rails.root}/config/infisecure.yml")
   file_config = YAML.load(file)
 
-  config.auth_code = file_config[Rails.env][:lnis_sub_code]
-  config.secret_key = file_config[Rails.env][:lnis_auth_header]
-  config.api_url = file_config[Rails.env][:lnis_api_url]
+  config.auth_code = file_config["auth_code"]
+  config.secret_key = file_config["secret_key"]
+  config.api_url = file_config["api_url"]
 end
