9c0d02ba081807a7c91f8c6b7b04b157dc50183a
Satish <zsatish201@gmail.com>
2016-10-08 17:57:10 +0530

    restructure files to generator install after gem install

--- lib/generators/infisecure_generator.rb
+++
@@ -1,7 +0,0 @@
-class InfisecureGenerator < Rails::Generators::Base
-  source_root(File.expand_path(File.dirname(__FILE__))
-  def copy_initializer_file
-    copy_file 'infisecure_credentials.rb', 'config/initializers/infisecure_credentials.rb'
-    copy_file 'infisecure.yml', 'config/infisecure.yml'
-  end
-end
