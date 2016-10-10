1a50223319357a5a29b5e1ba31cbad58c4003462
Satish <zsatish201@gmail.com>
2016-10-10 15:34:31 +0530

    refactor js code

--- lib/infisecure/view_helpers.rb
+++ lib/infisecure/view_helpers.rb
@@ -6,7 +6,7 @@ module Infisecure
 
     def render_infisecurejs options={}
       binding.local_variable_set(:options, options.to_json)
-      template_file = ::File.read(::File.expand_path('../templates/infisecure.js.erb', __FILE__))
+      template_file = ::File.read(::File.expand_path('../templates/infisecure.erb', __FILE__))
       raw ERB.new(template_file).result(binding)
     end
   end
