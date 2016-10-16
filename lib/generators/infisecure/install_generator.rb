9aaaa2203fbe2c4b1c2b7e345873fc8550f73dfb
Satish <zsatish201@gmail.com>
2016-10-16 20:19:48 +0530

    added template

--- lib/generators/infisecure/install_generator.rb
+++ lib/generators/infisecure/install_generator.rb
@@ -23,8 +23,8 @@ module Infisecure
   		end
       puts "Sid: "
       @auth_code = gets.chomp
-      binding.pry
-	    copy_file '../templates/infisecure_config.rb', 'config/initializers/infisecure_config.rb', :force => true
+      
+	    template '../templates/infisecure_config.rb', 'config/initializers/infisecure_config.rb', :force => true
 	    # copy_file 'infisecure.yml', 'config/infisecure.yml', :force => true
 	  end
 	end
